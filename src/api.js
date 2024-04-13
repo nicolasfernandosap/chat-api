1ecc97a8eaec3046493e1f9dd9a6afc273e75d99
nicolas fernando <nicolasfernandosap@gmail.com>
2024-04-13 09:31:48 -0300

    Atualizando salas api.js

--- src/api.js
+++ src/api.js
@@ -56,6 +56,21 @@ app.use('/sala/entrar', router.put('/sala/entrar', async (req, res) => {
 
 }))
 
+// Criar sala
+app.use('/sala/criar', router.post('/sala/criar', async (req, res) => {
+    if (!token.checktoken(req.headers.token, req.headers.iduser, req.headers.nick))
+        return false;
+
+    // Extrair os dados necessários do corpo da requisição
+    const { nome, tipo } = req.body;
+
+    // Chamar o controlador para criar a sala
+    let resp = await salaController.criarSala(req.headers.iduser, nome, tipo);
+    res.status(200).send(resp);
+}))
+
+
+
 // Enviar mensagens
 app.use('/sala/mensagem/', router.post('/sala/mensagem', async (req, res) => {
     if (!token.checktoken(req.headers.token, req.headers.iduser, req.headers.nick))
@@ -71,5 +86,27 @@ app.use('/sala/mensagens/', router.get('/sala/mensagens', async (req, res) => {
     let resp = await salaController.buscarMensagens(req.query.idsala, req.query.timestamp);
     res.status(200).send(resp);
 }))
+
+//Sair da sala
+
+app.use('/sala/sair', router.put('/sala/sair', async (req, res) => {
+    if (!token.checktoken(req.headers.token, req.headers.iduser, req.headers.nick))
+        return false;
+    console.log(req.headers);
+    let resp = await salaController.sairSala(req.headers.iduser, req.query.idsala);
+    res.status(200).send(resp);
+}));
+
+// sair do chat
+app.use('/sair', router.put('/sair', async (req, res) => {
+    if (!token.checktoken(req.headers.token, req.headers.iduser, req.headers.nick))
+        return false;
+
+    const usuarioController = require('./controllers/usuarioController');
+    let resp = await usuarioController.sairChat(req.headers.iduser);
+
+    res.status(200).send(resp);
+}));
+
 
 module.exports=app;
